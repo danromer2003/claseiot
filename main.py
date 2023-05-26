c7e93ad5a108b348b69ea1f39d1a7948172d18f7
danromer2003 <134085989+danromer2003@users.noreply.github.com>
2023-05-26 02:22:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,53 @@ app=FastAPI()
 @app.get("/")
 def index(): 
     return "Hola a todos, quieres saber de spider man"
+    
+@app.get("/Pokemon/{num}")
+def pokemon(num):
+    pokemons={
+       "1":"Bulbasur",
+       "2":"Ivysaur",
+       "3":"Venasur",
+       "4":"Charmander",
+        
+       }
+    return pokemons{num}
+
+@app.get("/Conversor_CaF/{C}")
+def conversorCaf(C):
+       try: 
+                C=float(C)
+                TF=C*(9/5) + 32
+                return f"La temperatura es de {TF} grados farenheit"
+            except: 
+                     return "Entrada invalida"
+                
+@app.get("/RevisarEdad/{E1}/{E2}")
+def revisar_edades(E1,E2):
+    E1=int(E1)
+    E2=int(E2)
+    if E1>E2+30:
+        return "Podrias ser su Abuelo"
+    elif E1>E2+15:
+        return "Podrias ser su padre"
+    elif E1>E2:
+        return "eres mayor"
+    elif E2>E1+30:
+        return "podria ser tu abuelo"
+    elif E2>E1+15:
+        return "Podria ser tu padre"
+    elif E2>E1:
+        return "Es mayor que tu"
+    else:
+        return "tienen la misma edad"
+    
+class Item(BaseModel):
+    name: str 
+    description: str
+    price: float
+        
+@app.post("items")
+            
+             
+            
+     
